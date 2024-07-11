d2388e9964a4c4628209b840217243fbd2e2d142
Md Wahid Rahman <wahidurrahmanraza@gmail.com>
2024-07-11 14:18:16 +0600

    Update filter, map and reduce

--- Iterations/filter_map_and_reduce.js
+++ Iterations/filter_map_and_reduce.js
@@ -37,3 +37,52 @@ const newNum = myNumbers.map((num) => num*10).map((num) => num+5).filter((num) =
 //* The above line task is called chaining. 
 console.log(newNum);
 //*=====================================================================================*//
+
+
+//*Reduce: (Calculates the values in the array)(It works like a loop)
+//*==================================================================
+const arr1 = [1, 2, 3, 4]
+
+const initialValue = 0
+const sumWithInitial = arr1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue, initialValue 
+);
+
+console.log(`The sum of arr1 is ${sumWithInitial}.`);
+
+//*Another Example:
+const arr2 = [1, 2, 3]
+
+const res = arr2.reduce(
+  (a, b) => {
+    console.log(`accumulator is ${a} and currentValue is ${b}`);
+    return a+b}, 0
+)
+console.log(`The second result is ${res}`);
+//*Another Example:
+const arr3 = [5, 6, 7]
+const res2 = arr3.reduce( (a, b) => (a+b), 0 )
+console.log(`The third result is ${res2}.`);
+
+//* Real project Example:
+const shoppingCart = [
+  {
+    itemName: "Python Course",
+    price: 299
+  },
+  {
+    itemName: "js Course",
+    price: 999
+  },
+  {
+    itemName: "cpp Course",
+    price: 2000
+  },
+  {
+    itemName: "java Course",
+    price: 888
+  }
+]
+
+const sumOfCarts = shoppingCart.reduce( (a, c) => (a + c.price), 0)
+console.log(`The sum of Carts is ${sumOfCarts}!!`);
